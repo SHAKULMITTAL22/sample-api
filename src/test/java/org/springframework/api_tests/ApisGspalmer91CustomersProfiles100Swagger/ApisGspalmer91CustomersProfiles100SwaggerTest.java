3879fdf7a1686904d58b2e255d263f71fc536229
SHAKULMITTAL22 <80421896+SHAKULMITTAL22@users.noreply.github.com>
2025-07-02 11:33:53 +0530

    API test (Using API Spec) generated by RoostGPT
    Using AI Model Inference

---
+++ src/test/java/org/springframework/api_tests/ApisGspalmer91CustomersProfiles100Swagger/ApisGspalmer91CustomersProfiles100SwaggerTest.java
@@ -0,0 +1,23 @@
+
+  package org.springframework.api_tests;
+
+  import com.intuit.karate.Results;
+  import com.intuit.karate.Runner;
+  // import com.intuit.karate.http.HttpServer;
+  // import com.intuit.karate.http.ServerConfig;
+  import org.junit.jupiter.api.Test;
+
+  import static org.junit.jupiter.api.Assertions.assertEquals;
+
+  class ApisGspalmer91CustomersProfiles100SwaggerTest {
+
+      @Test
+      void testAll() {
+          String urlbase = System.getenv().get("URL_BASE");
+          Results results = Runner.path("src/test/java/org/springframework/api_tests/ApisGspalmer91CustomersProfiles100Swagger")
+                  .systemProperty("URL_BASE", urlbase)
+                  .reportDir("testReport").parallel(1);
+          assertEquals(0, results.getFailCount(), results.getErrorMessages());
+      }
+
+  }
